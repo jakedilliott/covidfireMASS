326aa330cbd52b01c58c4b1908d7d2813c0f7c53
Jake Dilliott <jdilliott@protonmail.com>
2021-03-26 17:03:28 -0600

    functionality for role based vaccine plans

---
+++ R/find_role_type.R
@@ -0,0 +1,58 @@
+# covidfireMASS/R/find_overhead.R
+# author: Jake Dilliott
+# year: 2021
+
+
+#' Assign each firefighter agent a role "type"
+#'
+#' Inspired by a fizzbuzz, this function finds which module types each agent has
+#' been on and prints them out
+#'
+#' @param mod_data module assignment data
+#'
+#' @return character vector of agent role/module "types" (Overhead, OverheadCrew,
+#'     OverheadEngine, OverheadCrewEngine, Crew, CrewEngine, Engine)
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' find_role_type(mod_id_2017)
+#' }
+find_role_type <- function(mod_data) {
+  pb <- progress::progress_bar$new(
+    format = "[:bar] :current/:total (:percent)",
+    total = nrow(mod_data)
+  )
+  sapply(
+    1:nrow(mod_data),
+    function(row) {
+      agent_mods <- unique(as.list(mod_data[row, -(1:2)]))
+      agent_type <- find_mod(agent_mods)
+      pb$tick()
+      return(agent_type)
+    }
+  )
+}
+
+is_mod <- function(data, mod) {
+  mod_expr <- paste0("^", mod, "-") # "^O-"
+
+  grep_call <- grep(mod_expr, data)
+  if (length(grep_call) > 0) {
+    stats::setNames(TRUE, mod)
+  } else {
+    stats::setNames(FALSE, mod)
+  }
+}
+
+find_mod <- function(data) {
+  mods <- unlist(lapply(c("O", "C", "E"), function(x) is_mod(data, x)))
+
+  out <- ""
+
+  if (mods["O"]) out <- paste0(out, "Overhead")
+  if (mods["C"]) out <- paste0(out, "Crew")
+  if (mods["E"]) out <- paste0(out, "Engine")
+
+  return(out)
+}
---
+++ R/vaccine_fxns.R
@@ -0,0 +1,97 @@
+# covidfireMASS/R/vaccine_fxns.R
+# author: Jake Dilliott
+# year: 2021
+
+
+
+#' Plan vaccines based on gacc information
+#'
+#' Uses the approx function to calculate the number of agents that need to be
+#' vaccinated during each time step rounded to the nearest whole number.
+#'
+#' @param table contigency table created by "table" containing frequency of
+#'     either gaccs or roles
+#' @param x numeric vector, time steps
+#' @param y numeric vector, values from 0-1 specifying proportion of each gacc
+#'     to vaccinate at times corresponding to x
+#'
+#' @return Vaccine plan where row index = time, colnames = gacc, values = number
+#'     of agents to vaccinate
+#' @export
+plan_vaccines <- function(table, x, y) {
+  if (length(x) < 2 | length(y) < 2) {
+    stop("Inputs 'x' and 'y' must be length 2 or greater")
+  }
+  if (length(x) != length(y)) {
+    stop("Inputs 'x' and 'y' must have equal length")
+  }
+
+  vax_plan <- lapply(
+    table,
+    function(pop) {
+      to_vax <- pop * y
+      round(stats::approx(x = x, y = to_vax, n = utils::tail(x, 1))$y)
+    }
+  )
+  vax_plan <- as.data.frame(vax_plan)
+  names(vax_plan) <- names(table)
+
+  if ("overhead" %in% names(table)) {
+    list(method = "role", plan = vax_plan)
+  } else {
+    list(method = "gacc", plan = vax_plan)
+  }
+}
+
+
+
+#' Find out which agents to vaccinate using gacc information
+#' @param input_df data frame containing agent roster
+#' @param overhead_ids res ids of overhead agents for the purpose of vaccination
+#' @param method method of vaccine delivery, either by role or by gacc
+#' @param plan vaccination plan
+#' @param efficacy proportion of vaccinated agents the gain immunity
+#' @returns nested list of res_ids; output$vaccinated and output$immune
+vaccinate <- function(input_df, overhead_ids = NULL, method, plan, efficacy) {
+  if (method == "role") {
+    if (is.null(overhead_ids)) {
+      stop("Overhead agents not specified")
+    }
+    input_df$role <- ifelse(input_df$res_id %in% overhead_ids, "overhead", "ground")
+    split_df <- split(input_df, input_df$role)
+  } else if (method == "gacc") {
+    split_df <- split(input_df, input_df$res_gacc)
+  } else {
+    stop("Incorrect vaccine distribution method specified")
+  }
+
+  vax_ids <- lapply(
+    split_df,
+    function(sub_pop) {
+      num_vaccinated <- length(which(sub_pop$vaccinated))
+      candidates <- sub_pop$res_id[!sub_pop$vaccinated &
+                                     !sub_pop$quarantined $
+                                     sub_pop$inc_id == 0]
+
+      if (method == "role") {
+        role <- sub_pop$role[1]
+        num_to_vax <- plan[1, role] - num_vaccinated
+      } else if (method == "gacc") {
+        home_gacc <- sub_pop$res_gacc[1]
+        num_to_vax <- plan[1, home_gacc] - num_vaccinated
+      }
+
+      if (length(candidates) < num_to_vax) {
+        candidates
+      } else {
+        sample(candidates, num_to_vax)
+      }
+    }
+  )
+  vax_ids <- as.numeric(unlist(vax_ids))
+  # This method is deterministic, it guarantees that the proportion of
+  # immune agents is equal to the vax_efficacy
+  immune_ids <- sample(vax_ids, round(length(vax_ids) * efficacy))
+
+  return(list(vaccinated=vax_ids, immune=immune_ids))
+}
